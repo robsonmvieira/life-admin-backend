e1be147a1334ccd4692833bc5a8821e739cb2490
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 11:18:38 -0300

    :seedling: auto generate migrations files

---
+++ src/infra/database/migrations/1599262094133-createCompany.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createCompany1599262094133 implements MigrationInterface {
+    name = 'createCompany1599262094133'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "categories" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "UQ_8b0be371d28245da6e4f4b61878" UNIQUE ("name"), CONSTRAINT "PK_24dbc6126a28ff948da33e97d3b" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "categories"`);
+    }
+
+}
---
+++ src/infra/database/migrations/1599480034223-RemoveTitleFromProduct.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class RemoveTitleFromProduct1599480034223 implements MigrationInterface {
+    name = 'RemoveTitleFromProduct1599480034223'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "products_sale" DROP CONSTRAINT "UQ_3e42803d8a4e2c891f652ddd5a2"`);
+        await queryRunner.query(`ALTER TABLE "products_sale" DROP COLUMN "title"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "products_sale" ADD "title" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "products_sale" ADD CONSTRAINT "UQ_3e42803d8a4e2c891f652ddd5a2" UNIQUE ("title")`);
+    }
+
+}
