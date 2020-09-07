a3d90bb896e925ea3a7cc96be9e331c14c0474d1
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 11:20:29 -0300

    :sparkles: add product routes

---
+++ src/infra/http/routes/product-sale-routes.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express'
+import ProductSaleController from '../../../api/product-sale-controller'
+const routes = Router()
+
+const categoryCtr = new ProductSaleController()
+
+routes.get('/', categoryCtr.index)
+routes.get('/:id', categoryCtr.one)
+routes.post('/', categoryCtr.create)
+routes.put('/:id', categoryCtr.update)
+routes.delete('/:id', categoryCtr.remove)
+
+export default routes
