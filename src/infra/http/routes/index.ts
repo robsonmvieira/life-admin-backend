6b8a2146767d9bdf2d1bc9e1fe73b5a612afdf81
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 19:41:29 -0300

    :sparkles: add new route resource

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express'
 import categoryRoutes from './category-routes'
 import productRoutes from './product-sale-routes'
+import adminRoutes from './admin-router'
 const routes = Router()
 
 routes.use('/categories', categoryRoutes)
 routes.use('/products', productRoutes)
+routes.use('/admin', adminRoutes)
 
 export default routes
