8472db12878388bc5080a2f2f264283230b2b80f
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 11:27:13 -0300

    :sparkles: add handler to get product

---
+++ src/modules/products/services/queries/getone-product-handler.ts
@@ -0,0 +1,18 @@
+import { injectable, inject } from "tsyringe";
+import ProductSaleRepository from "@infra/repositories/productSaleRepository";
+import ProductSale from "@modules/products/models/product";
+
+@injectable()
+export default class GetOneProductHandler{
+
+  /**
+   *
+   */
+  constructor(@inject("ProductSaleRepository") private repo: ProductSaleRepository) {
+    
+  }
+  async handler(data: string): Promise<ProductSale| undefined>{
+    return await this.repo.one(data)
+  }
+}
+
