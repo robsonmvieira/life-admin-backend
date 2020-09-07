dbad726ac8b884391f2e3b4b0bc359406fcfeb2e
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 19:42:31 -0300

    :sparkles: add new method for category controller

--- src/api/category-controller.ts
+++ src/api/category-controller.ts
@@ -6,6 +6,7 @@ import GetCategoryHandler from "@modules/categories/services/queries/getone-cate
 import CreateCategoryHandler from "@modules/categories/services/commands/create-category-handler";
 import UpdadeCategoryHandler from "@modules/categories/services/commands/update-category-handler";
 import RemoveCategoryHandler from "@modules/categories/services/commands/remove-category-handler";
+import GetCategoryByNameHandler from "@modules/categories/services/queries/get-by-name-category-handler";
 
 export default class CategoryController implements ControllerBase {
   async index(req: Request, res: Response): Promise<Response> {
@@ -40,6 +41,13 @@ export default class CategoryController implements ControllerBase {
     const service = container.resolve(RemoveCategoryHandler)
     const isDeletedCategory = await service.handler(id)
     return res.status(200).json(isDeletedCategory)
+
+  }
+  async findByName(req: Request, res: Response): Promise<Response> {
+    const data = req.params.name
+    const service = container.resolve(GetCategoryByNameHandler)
+    const hasCategory = await service.handler(data)
+    return res.status(200).json(hasCategory)
 
   }
   
