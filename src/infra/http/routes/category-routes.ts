31088cd3b9af08ded32528555e1ed15e090e867a
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 19:49:26 -0300

    :sparkles: add new endpoint to routes

--- src/infra/http/routes/category-routes.ts
+++ src/infra/http/routes/category-routes.ts
@@ -7,8 +7,10 @@ const categoryCtr = new CategoryController()
 
 routes.get('/', categoryCtr.index)
 routes.get('/:id', categoryCtr.one)
+routes.get('/findByName/:name', categoryCtr.findByName)
 routes.post('/', categoryCtr.create)
 routes.put('/:id', categoryCtr.update)
 routes.delete('/:id', categoryCtr.remove)
+
 
 export default routes
