b83eab83f580f04a8dea9d247efc940efa6f35dc
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 12:22:10 -0300

    :sparkles: add enum to descount profile consulter

---
+++ src/modules/products/profile-consulter.ts
@@ -0,0 +1,6 @@
+export enum ProfileConsulter {
+  FromZeroToFourHundredNinetyNine = 25,
+  FromFiveHundredToNineHundredNinetyNine = 35,
+  FromOneThousandToThreeThousandNineHundredNinetyNine = 42,
+	MoreThanFourThousand= 50
+}
