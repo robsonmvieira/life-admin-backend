067f932572a73aa11216ed7e9bf1464a78dadf9d
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 11:22:08 -0300

    :sparkles: add category model

---
+++ src/modules/categories/models/category.ts
@@ -0,0 +1,13 @@
+import Base from "@shared/baseEntity/entity";
+import { Column, Entity, OneToMany } from "typeorm";
+import ProductSale from "@modules/products/models/product";
+
+@Entity('categories')
+export default class Category extends Base{
+  
+  @Column({nullable: false, unique: true})
+  name: string
+
+  @OneToMany(type => ProductSale, products => products.category)
+  products: ProductSale[]
+}
