cfa59fa705450635e5a0216e27774ee16783281e
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 11:21:38 -0300

    :sparkles: add category contract repository

---
+++ src/modules/categories/interfaces/ICategoryRepository.ts
@@ -0,0 +1,12 @@
+import Category from "../models/category";
+import CreateCategoryInput from "../dtos/create-category-input";
+import UpdateCategoryInput from "../dtos/update-category-input";
+
+export default interface ICategoryRepository{
+  create(data: CreateCategoryInput): Promise<Category>
+  index(): Promise<Category[]>
+  one(id: string): Promise<Category | undefined>
+  update(id: string, data: UpdateCategoryInput): Promise<Category | undefined>
+  delete(id: string): Promise<boolean | undefined>
+  
+}
