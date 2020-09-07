72f29b0229f8e60e46cadc5d249626523b97d1e9
Robson Maia <robsonmvieira@gmail.com>
2020-09-07 12:29:52 -0300

    :building_construction: extract number calcule

--- src/modules/products/services/commands/create-product-sale-handler.ts
+++ src/modules/products/services/commands/create-product-sale-handler.ts
@@ -2,6 +2,7 @@ import CreateProductSaleInput from "@modules/products/dtos/create-product-sale-i
 import ProductSale from "@modules/products/models/product";
 import { injectable, inject } from "tsyringe";
 import ProductSaleRepository from "@infra/repositories/productSaleRepository";
+import { ProfileConsulter } from "@modules/products/profile-consulter";
 
 @injectable()
 export default class CreateProductSalerHandler {
@@ -11,8 +12,11 @@ export default class CreateProductSalerHandler {
   }
 
   async handler(data: CreateProductSaleInput): Promise<ProductSale>{
-    // const price_base = data.price_suggest
-    const result = await this.repo.create(data)
-    return result
+    const price_base = data.price_suggest
+    data.from_zero_to_four_hundred_ninety_nine= price_base * (ProfileConsulter.FromZeroToFourHundredNinetyNine /100)
+    data.from_five_hundred_to_nine_hundred_ninety_nine = price_base * (ProfileConsulter.FromFiveHundredToNineHundredNinetyNine /100)
+    data.from_one_thousand_to_three_thousand_nine_hundred_ninety_nine = price_base * (ProfileConsulter.FromOneThousandToThreeThousandNineHundredNinetyNine /100)
+    data.more_than_four_thousand = price_base * (ProfileConsulter.MoreThanFourThousand /100)
+    return await this.repo.create(data)
   }
 }
