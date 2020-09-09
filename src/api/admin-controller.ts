b7fb9b27beb795d0b2d86e9b6f4e93c9fc573126
Robson Maia <robsonmvieira@gmail.com>
2020-09-08 21:24:15 -0300

    :sparkles: add new controller method to admin

--- src/api/admin-controller.ts
+++ src/api/admin-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import XlsxToJsonParser from "@infra/parserxlsFiles/converterXlsxToJson";
+import { container } from "tsyringe";
+import GetProductSaleLengthFromDbHandler from "@modules/products/services/queries/get-total-from-db-handler";
 export default class AdminController {
 
   async convertXlsx(req: Request, res: Response): Promise<Response> { 
@@ -7,4 +9,11 @@ export default class AdminController {
     xlsx.parserToJson()
     return res.status(200).json({message: 'loaded'})
   }
+
+  async productsListLengthFromDb(req: Request, res: Response): Promise<Response> { 
+    const productHandler = container.resolve(GetProductSaleLengthFromDbHandler)
+    const total = await productHandler.handler()
+    return res.status(200).json(total)
+  }
+  
 }
