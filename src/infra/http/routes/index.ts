55326cd1940e5f8d4fe7fc765634a94f649171c2
Robson Maia <robsonmvieira@gmail.com>
2020-09-09 07:34:11 -0300

    :sparkles: add sale routes

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -2,10 +2,13 @@ import {Router} from 'express'
 import categoryRoutes from './category-routes'
 import productRoutes from './product-sale-routes'
 import adminRoutes from './admin-router'
+import saleRoutes from './sale-router'
 const routes = Router()
 
 routes.use('/categories', categoryRoutes)
 routes.use('/products', productRoutes)
 routes.use('/admin', adminRoutes)
+routes.use('/sales', saleRoutes)
+
 
 export default routes
