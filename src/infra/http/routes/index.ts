553b049424d62b64f5c5911e3ebfd04c257f1f18
Robson Maia <robsonmvieira@gmail.com>
2020-09-09 07:54:17 -0300

    :sparkles: update index root routes

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import categoryRoutes from './category-routes'
 import productRoutes from './product-sale-routes'
 import adminRoutes from './admin-router'
 import saleRoutes from './sale-router'
+import userRoutes from './user-router'
 const routes = Router()
 
 routes.use('/categories', categoryRoutes)
 routes.use('/products', productRoutes)
 routes.use('/admin', adminRoutes)
+routes.use('/users', userRoutes)
 routes.use('/sales', saleRoutes)
 
 
