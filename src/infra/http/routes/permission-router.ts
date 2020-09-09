9eab835a921b7066d517118e968fabe3e36779e5
Robson Maia <robsonmvieira@gmail.com>
2020-09-09 20:17:14 -0300

    :sparkles: add permission route

---
+++ src/infra/http/routes/permission-router.ts
@@ -0,0 +1,15 @@
+import {Router} from 'express'
+import PermissionController from '@api/permission-controller'
+
+const routes = Router()
+
+const permissionController = new PermissionController()
+
+routes.get('/', permissionController.index)
+routes.get('/:id', permissionController.one)
+routes.post('/', permissionController.create)
+routes.put('/:id', permissionController.update)
+routes.delete('/:id', permissionController.remove)
+
+
+export default routes
