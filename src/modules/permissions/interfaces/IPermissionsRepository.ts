1650062f1f727d76f4508b6666ba14f7be5e4364
Robson Maia <robsonmvieira@gmail.com>
2020-09-12 08:23:30 -0300

    :shirt: apply linter

--- src/modules/permissions/interfaces/IPermissionsRepository.ts
+++ src/modules/permissions/interfaces/IPermissionsRepository.ts
@@ -1,11 +1,14 @@
-import CreatePermissionInput from "../dtos/create-permission-input";
-import Permission from "../models/permission";
-import UpdatePermissionInput from "../dtos/update-permission-input";
+import CreatePermissionInput from '../dtos/create-permission-input'
+import Permission from '../models/permission'
+import UpdatePermissionInput from '../dtos/update-permission-input'
 
-export default interface IPermissionsRepository{
-  create(data: CreatePermissionInput):Promise<Permission>
-  index():Promise<Permission[]>
-  one(id: string):Promise<Permission| undefined>
-  update(id: string, data: UpdatePermissionInput):Promise<Permission | undefined>
+export default interface IPermissionsRepository {
+  create(data: CreatePermissionInput): Promise<Permission>
+  index(): Promise<Permission[]>
+  one(id: string): Promise<Permission | undefined>
+  update(
+    id: string,
+    data: UpdatePermissionInput
+  ): Promise<Permission | undefined>
   remove(id: string): Promise<boolean>
 }
