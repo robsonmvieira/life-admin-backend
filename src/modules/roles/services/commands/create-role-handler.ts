2f3402b648546b477b70eb5b0bab2f649de5aea8
Robson Maia <robsonmvieira@gmail.com>
2020-09-12 08:20:21 -0300

    :hammer: change DI signature

--- src/modules/roles/services/commands/create-role-handler.ts
+++ src/modules/roles/services/commands/create-role-handler.ts
@@ -1,19 +1,17 @@
-import { injectable, inject } from "tsyringe";
+import { injectable, inject } from 'tsyringe'
 import AppError from '@infra/errors/AppError'
-import RoleRepository from "@infra/repositories/roleRepository";
-import Role from "@modules/roles/models/role";
-import CreateRoleInput from "@modules/roles/dtos/create-roles-input";
+import Role from '@modules/roles/models/role'
+import CreateRoleInput from '@modules/roles/dtos/create-roles-input'
+import IRoleRepository from '@modules/roles/interfaces/IRoleRepository'
 @injectable()
-export default class CreateRoleHandler{
+export default class CreateRoleHandler {
+  constructor(@inject('RoleRepository') private repo: IRoleRepository) {}
 
-  constructor(@inject("RoleRepository") private repo: RoleRepository) {}
-
-  async handler(data: CreateRoleInput): Promise<Role>{
-
+  async handler(data: CreateRoleInput): Promise<Role> {
     try {
       return await this.repo.create(data)
     } catch (error) {
-      throw new AppError("Os dados da Nova role estão incorretos", 400, error)
+      throw new AppError('Os dados da Nova role estão incorretos', 400, error)
     }
   }
 }
