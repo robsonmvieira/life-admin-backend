5781c63be4f0ea79c671408aec31193df3d7c4a6
Robson Maia <robsonmvieira@gmail.com>
2020-09-12 08:24:50 -0300

    :hammer: change method position params

--- src/shared/encrypter/implementation/encripter.ts
+++ src/shared/encrypter/implementation/encripter.ts
@@ -1,11 +1,15 @@
 import bcryptjs from 'bcryptjs'
-import IHashPassword from "../contract/encrypter";
+import IHashPassword from '../contract/encrypter'
 
-export default class HashPassword implements IHashPassword{
-  async comparePassword(hashedPassword: string, plainPassword: string): Promise<boolean> {
+export default class HashPassword implements IHashPassword {
+  async comparePassword(
+    plainPassword: string,
+    hashedPassword: string
+  ): Promise<boolean> {
     const result = await bcryptjs.compare(plainPassword, hashedPassword)
     return result
   }
+
   async hashPassword(plainPassword: string): Promise<string> {
     const data = bcryptjs.hash(plainPassword, 8)
     return data
