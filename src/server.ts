8331c3edb4bd81d88021fdd9568cc2355baa8cf1
Robson Maia <robsonmvieira@gmail.com>
2020-09-12 00:18:22 -0300

    :goal_net: error config

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,33 @@
-import "reflect-metadata";
-import "./infra/database/connection"
-import "./infra/container"
+import 'reflect-metadata'
+import './infra/database/connection'
+import './infra/container'
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import routes from './infra/http/routes'
-import AppError from "@infra/errors/AppError";
+import AppError from '@infra/errors/AppError'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(routes)
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+        error: error.erroMessage
+      })
+    }
 
-  if(error instanceof AppError){
-    return response.status(error.statusCode).json({
+    console.error(error)
+    return response.status(500).json({
       status: 'error',
-      message: error.message,
-      error: error.erroMessage
+      message: 'Internal server Error'
     })
   }
-
-  console.error(error);
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server Error'
-  })
-})
+)
 
 app.listen(8000, () => console.log('server on port localhost:8000'))
