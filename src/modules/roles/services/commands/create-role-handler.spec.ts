4fb82b29ccba31af1e301ed3ff44824bb6dadfb6
Robson Maia <robsonmvieira@gmail.com>
2020-09-12 08:27:05 -0300

    :heavy_check_mark: create role test done

---
+++ src/modules/roles/services/commands/create-role-handler.spec.ts
@@ -0,0 +1,27 @@
+import AppError from '@infra/errors/AppError'
+import FakeRoleRepository from '@infra/repositories/fakes/roleRepository'
+// import FakeRoleRepository from '@infra/repositories/fakes/roleRepository'
+import CreateRoleHandler from './create-role-handler'
+
+const roleRepository = new FakeRoleRepository()
+const createRole = new CreateRoleHandler(roleRepository)
+describe('create role Handler', () => {
+  it('should be able  create a new role', async () => {
+    const data = {
+      description: 'any_description',
+      name: 'any_name',
+      slug: 'any-slug'
+    }
+    const role = await createRole.handler(data)
+    expect(role).toHaveProperty('id')
+  })
+
+  // it('should return error if description does not provide', async () => {
+  //   expect(
+  //     await createRole.handler({
+  //       name: 'any_name',
+  //       slug: 'any-slug'
+  //     })
+  //   ).rejects.toBeInstanceOf(AppError)
+  // })
+})
