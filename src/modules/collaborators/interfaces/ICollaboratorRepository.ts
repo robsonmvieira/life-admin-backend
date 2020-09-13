d88b50a40bf9e00ba978045a9b9d1b6ea79b4430
Robson Maia <robsonmvieira@gmail.com>
2020-09-13 10:23:30 -0300

    :sparkles: add Collaborator Repository contract

---
+++ src/modules/collaborators/interfaces/ICollaboratorRepository.ts
@@ -0,0 +1,15 @@
+import Collaborator from '../models/collaborator'
+import CreateCollaboratorInput from '../dtos/create-collaborator-input'
+import UpdateCollaboratorInput from '../dtos/update-collaborator-input'
+
+export default interface ICollaboratorRepository {
+  create(data: CreateCollaboratorInput): Promise<Collaborator>
+  index(): Promise<Collaborator[]>
+  one(id: string): Promise<Collaborator | undefined>
+  update(
+    id: string,
+    data: UpdateCollaboratorInput
+  ): Promise<Collaborator | undefined>
+  remove(id: string): Promise<boolean>
+  save(data: Collaborator): Promise<Collaborator>
+}
