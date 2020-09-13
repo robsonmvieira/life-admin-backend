1827032be3f4dbf5760c2d0bd4d74d318b061cec
Robson Maia <robsonmvieira@gmail.com>
2020-09-13 10:32:37 -0300

    :shirt: apply linter

--- src/modules/users/services/commands/remove-user-handler.ts
+++ src/modules/users/services/commands/remove-user-handler.ts
@@ -1,17 +1,19 @@
-import { injectable, inject } from "tsyringe";
+import { injectable, inject } from 'tsyringe'
 import AppError from '@infra/errors/AppError'
-import UserRepository from "@infra/repositories/userRepository";
+import IUserRepository from '@modules/users/interfaces/IUserRepository'
 @injectable()
-export default class RemoveUserHandler{
+export default class RemoveUserHandler {
+  constructor(@inject('UserRepository') private repo: IUserRepository) {}
 
-  constructor(@inject("UserRepository") private repo: UserRepository) {}
-
-  async handler(data: string): Promise<boolean>{
-
+  async handler(data: string): Promise<boolean> {
     try {
       return await this.repo.remove(data)
     } catch (error) {
-      throw new AppError("Não foi possível deltar o Usuário solicitado", 400, error)
+      throw new AppError(
+        'Não foi possível deltar o Usuário solicitado',
+        400,
+        error
+      )
     }
   }
 }
