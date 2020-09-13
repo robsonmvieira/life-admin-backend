d71ce26f1a2ce73d9adc78d9993e0291c4b2a59d
Robson Maia <robsonmvieira@gmail.com>
2020-09-13 10:28:20 -0300

    :hammer: add new fields

--- src/modules/users/dtos/update-user-input.ts
+++ src/modules/users/dtos/update-user-input.ts
@@ -1,9 +1,14 @@
-export default interface UpdateUserInput{
-  name: string,
-  password: string,
-  email: string
+import Permission from '@modules/permissions/models/permission'
+import Role from '@modules/roles/models/role'
+
+export default interface UpdateUserInput {
+  name?: string
+  password?: string
+  email?: string
   isAdmin?: boolean
   isActive?: boolean
-  roles?: string
-  permissions?: string
+  rolesId?: string
+  permissionsId?: string
+  roles?: Role[]
+  permissions?: Permission[]
 }
