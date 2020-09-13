a145c3e0dff59993f615452a80e5cf1dfaeebd1d
Robson Maia <robsonmvieira@gmail.com>
2020-09-13 10:25:07 -0300

    :sparkles: add collaborator getOne handler

---
+++ src/modules/collaborators/services/queries/getone-collaborator-handler.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from 'tsyringe'
+import AppError from '@infra/errors/AppError'
+
+import User from '@modules/users/models/user'
+import UserRepository from '@infra/repositories/userRepository'
+import ICollaboratorRepository from '@modules/collaborators/interfaces/ICollaboratorRepository'
+import Collaborator from '@modules/collaborators/models/collaborator'
+
+@injectable()
+export default class GetOneCollaboratorHandler {
+  constructor(
+    @inject('CollaboratorRepository') private repo: ICollaboratorRepository
+  ) {}
+
+  async handler(id: string): Promise<Collaborator | undefined> {
+    try {
+      return await this.repo.one(id)
+    } catch (error) {
+      throw new AppError(
+        'Um erro ocorreu ao tentar buscar o colaborador',
+        401,
+        error.TypeError
+      )
+    }
+  }
+}
