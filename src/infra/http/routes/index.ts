13c8a9632af662c690aa911ae9a6a70090e0f54a
Robson Maia <robsonmvieira@gmail.com>
2020-09-13 10:19:16 -0300

    :sparkles: update routes root

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -1,4 +1,4 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import categoryRoutes from './category-routes'
 import productRoutes from './product-sale-routes'
 import adminRoutes from './admin-router'
@@ -6,6 +6,7 @@ import saleRoutes from './sale-router'
 import userRoutes from './user-router'
 import permissionRoutes from './permission-router'
 import rolesRoutes from './role-router'
+import collaboratorRoutes from './collaborator-router'
 const routes = Router()
 
 routes.use('/categories', categoryRoutes)
@@ -15,6 +16,6 @@ routes.use('/users', userRoutes)
 routes.use('/sales', saleRoutes)
 routes.use('/permissions', permissionRoutes)
 routes.use('/roles', rolesRoutes)
-
+routes.use('/collaborators', collaboratorRoutes)
 
 export default routes
