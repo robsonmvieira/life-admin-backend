8eedb6edb79da2c9466d4c955212318fbb6eacc5
Robson Maia <robsonmvieira@gmail.com>
2020-09-15 13:27:55 -0300

    :recycle: improve business logic

--- src/modules/collaborators/dtos/update-collaborator-input.ts
+++ src/modules/collaborators/dtos/update-collaborator-input.ts
@@ -1,10 +1,7 @@
-import Permission from '@modules/permissions/models/permission'
-import Role from '@modules/roles/models/role'
-
 export default interface UpdateCollaboratorInput {
   password?: string
   email?: string
   position?: string
-  permissions?: Permission[]
-  roles?: Role[]
+  roles?: string
+  permissions?: string
 }
--- src/modules/users/dtos/create-user-input.ts
+++ src/modules/users/dtos/create-user-input.ts
@@ -1,14 +1,9 @@
-import Role from '@modules/roles/models/role'
-import Permission from '@modules/permissions/models/permission'
-
 export default interface CreateUserInput {
   name: string
   password: string
   email: string
   isAdmin?: boolean
   isActive?: boolean
-  rolesId?: string
-  permissionsId?: string
-  roles: Role[]
-  permissions: Permission[]
+  roles: string
+  permissions: string
 }
