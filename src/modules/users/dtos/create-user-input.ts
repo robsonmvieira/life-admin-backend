2be96f1df8894285c82f6b0f40ff50fdc6edac28
Robson Maia <robsonmvieira@gmail.com>
2020-09-22 19:40:59 -0300

    :art: change signature shape create user

--- src/modules/users/dtos/create-user-input.ts
+++ src/modules/users/dtos/create-user-input.ts
@@ -1,9 +1,12 @@
+import Permission from '@modules/permissions/models/permission'
+import Role from '@modules/roles/models/role'
+
 export default interface CreateUserInput {
   name: string
   password: string
   email: string
   isAdmin?: boolean
   isActive?: boolean
-  roles: string
-  permissions: string
+  roles: Role[]
+  permissions: Permission[]
 }
