83b2ded783641447d5d1c784ed0b2586bcd81a26
Robson Maia <robsonmvieira@gmail.com>
2020-09-22 20:59:11 -0300

    :shirt: apply linter

--- src/modules/products/profile-consulter.ts
+++ src/modules/products/profile-consulter.ts
@@ -2,5 +2,5 @@ export enum ProfileConsulter {
   FromZeroToFourHundredNinetyNine = 25,
   FromFiveHundredToNineHundredNinetyNine = 35,
   FromOneThousandToThreeThousandNineHundredNinetyNine = 42,
-	MoreThanFourThousand= 50
+  MoreThanFourThousand = 50
 }
