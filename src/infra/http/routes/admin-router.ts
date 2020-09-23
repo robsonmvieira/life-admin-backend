412a8948838fa7f18c639eb55ae5d7cb473554e9
Robson Maia <robsonmvieira@gmail.com>
2020-09-23 20:24:08 -0300

    :recycle: change get method to post

--- src/infra/http/routes/admin-router.ts
+++ src/infra/http/routes/admin-router.ts
@@ -4,8 +4,7 @@ import { Router } from 'express'
 const routes = Router()
 const adminCtl = new AdminController()
 
-
-routes.get('/converter', adminCtl.convertXlsx)
+routes.post('/converter', adminCtl.convertXlsx)
 routes.get('/listProductsFromDb', adminCtl.productsListLengthFromDb)
 
 export default routes
