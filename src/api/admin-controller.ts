2831e5756b7e38aeb74a48da7ed3023389c24dd5
Robson Maia <robsonmvieira@gmail.com>
2020-09-23 20:20:52 -0300

    :art: add parameter to parse table to json

--- src/api/admin-controller.ts
+++ src/api/admin-controller.ts
@@ -1,19 +1,22 @@
-import { Request, Response } from "express";
-import XlsxToJsonParser from "@infra/parserxlsFiles/converterXlsxToJson";
-import { container } from "tsyringe";
-import GetProductSaleLengthFromDbHandler from "@modules/products/services/queries/get-total-from-db-handler";
+import { Request, Response } from 'express'
+import XlsxToJsonParser from '@infra/parserxlsFiles/converterXlsxToJson'
+import { container } from 'tsyringe'
+import GetProductSaleLengthFromDbHandler from '@modules/products/services/queries/get-total-from-db-handler'
 export default class AdminController {
+  async convertXlsx(req: Request, res: Response): Promise<Response> {
+    const { id } = req.body
+    const xlsx = new XlsxToJsonParser()
 
-  async convertXlsx(req: Request, res: Response): Promise<Response> { 
-    const xlsx = new XlsxToJsonParser();
-    xlsx.parserToJson()
-    return res.status(200).json({message: 'loaded'})
+    await xlsx.parserToJson(id)
+    return res.status(200).json({ message: 'loaded' })
   }
 
-  async productsListLengthFromDb(req: Request, res: Response): Promise<Response> { 
+  async productsListLengthFromDb(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
     const productHandler = container.resolve(GetProductSaleLengthFromDbHandler)
     const total = await productHandler.handler()
     return res.status(200).json(total)
   }
-  
 }
