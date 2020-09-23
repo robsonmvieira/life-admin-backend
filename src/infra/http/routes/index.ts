6c1e94e527ff664eccf89bcc86d33b0383223bae
Robson Maia <robsonmvieira@gmail.com>
2020-09-23 20:25:39 -0300

    :art: change imports and names

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -3,7 +3,7 @@ import categoryRoutes from './category-routes'
 import productRoutes from './product-sale-routes'
 import adminRoutes from './admin-router'
 import saleRoutes from './sale-router'
-import userRoutes from './user-router'
+import ownerRoutes from './owner-router'
 import permissionRoutes from './permission-router'
 import rolesRoutes from './role-router'
 import collaboratorRoutes from './collaborator-router'
@@ -13,7 +13,7 @@ const routes = Router()
 routes.use('/categories', categoryRoutes)
 routes.use('/products', productRoutes)
 routes.use('/admin', adminRoutes)
-routes.use('/users', userRoutes)
+routes.use('/owners', ownerRoutes)
 routes.use('/sales', saleRoutes)
 routes.use('/permissions', permissionRoutes)
 routes.use('/roles', rolesRoutes)
