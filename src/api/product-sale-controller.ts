279ab79611ec8231746bd45202a93b2dbf500c73
Robson Maia <robsonmvieira@gmail.com>
2020-09-23 20:21:44 -0300

    :shirt: apply lint

--- src/api/product-sale-controller.ts
+++ src/api/product-sale-controller.ts
@@ -1,46 +1,45 @@
-import { Request, Response } from "express";
-import ControllerBase from "./controller-base";
-import { container } from "tsyringe";
-import ListProductSaleHandler from "@modules/products/services/queries/list-product-handler";
-import CreateProductSalerHandler from "@modules/products/services/commands/create-product-sale-handler";
-import UpdateProductSalerHandler from "@modules/products/services/commands/update-product-sale-handler";
-import RemoveProductSalerHandler from "@modules/products/services/commands/remove-product-sale-handler";
-import GetOneProductHandler from "@modules/products/services/queries/getone-product-handler";
-
+import { Request, Response } from 'express'
+import ControllerBase from './controller-base'
+import { container } from 'tsyringe'
+import ListProductSaleHandler from '@modules/products/services/queries/list-product-handler'
+import CreateProductSalerHandler from '@modules/products/services/commands/create-product-sale-handler'
+import UpdateProductSalerHandler from '@modules/products/services/commands/update-product-sale-handler'
+import RemoveProductSalerHandler from '@modules/products/services/commands/remove-product-sale-handler'
+import GetOneProductHandler from '@modules/products/services/queries/getone-product-handler'
+import { classToClass } from 'class-transformer'
 export default class ProductSaleController implements ControllerBase {
   async index(req: Request, res: Response): Promise<Response> {
-   const service = container.resolve(ListProductSaleHandler)
-   const categories = await service.handler()
-   return res.status(200).json(categories)
+    const service = container.resolve(ListProductSaleHandler)
+    const products = await service.handler()
+    return res.status(200).json(classToClass(products))
   }
+
   async one(req: Request, res: Response): Promise<Response> {
-    const {id} = req.params
+    const { id } = req.params
     const service = container.resolve(GetOneProductHandler)
-    const hasCategory = await service.handler(id)
-    return res.status(200).json(hasCategory)
-
+    const hasProduct = await service.handler(id)
+    return res.status(200).json(classToClass(hasProduct))
   }
+
   async create(req: Request, res: Response): Promise<Response> {
-    const newCategory = req.body
+    const newProduct = req.body
     const service = container.resolve(CreateProductSalerHandler)
-    const result = await service.handler(newCategory)
+    const result = await service.handler(newProduct)
     return res.status(201).json(result)
-
   }
+
   async update(req: Request, res: Response): Promise<Response> {
-    const {id} = req.params
+    const { id } = req.params
     const data = req.body
     const service = container.resolve(UpdateProductSalerHandler)
     const updatedCategory = await service.handler(id, data)
     return res.status(201).json(updatedCategory)
-
   }
+
   async remove(req: Request, res: Response): Promise<Response> {
-    const {id} = req.params
+    const { id } = req.params
     const service = container.resolve(RemoveProductSalerHandler)
     const isDeletedCategory = await service.handler(id)
     return res.status(200).json(isDeletedCategory)
-
   }
-  
 }
