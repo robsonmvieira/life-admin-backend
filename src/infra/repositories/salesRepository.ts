bd1c86e7cd36203074fe72fba6c4d59004e1fa11
Robson Maia <robsonmvieira@gmail.com>
2020-10-02 20:12:29 -0300

    :construction: implement item sales PDV repository

--- src/infra/repositories/salesRepository.ts
+++ src/infra/repositories/salesRepository.ts
@@ -1,4 +1,5 @@
-import { SaveSalesInput } from '@modules/sales/dtos/saveSales.input'
+import SaveSalesInput from '@modules/sales/dtos/saveSalesPDV'
+import { UpdateSalesInput } from '@modules/sales/dtos/updateSales.input'
 import ISalesPDVRepository from '@modules/sales/interfaces/ISalesPDVRepository'
 import SalesPDV from '@modules/sales/models/sale'
 import { getRepository, Repository } from 'typeorm'
@@ -6,6 +7,10 @@ export default class SalesPDVRepository implements ISalesPDVRepository {
   repo: Repository<SalesPDV>
   constructor() {
     this.repo = getRepository(SalesPDV)
+  }
+
+  async save(data: SalesPDV): Promise<SalesPDV> {
+    return this.repo.save(data)
   }
 
   async index(ownerId: string): Promise<SalesPDV[]> {
@@ -33,11 +38,26 @@ export default class SalesPDVRepository implements ISalesPDVRepository {
     throw new Error('Method not implemented.')
   }
 
-  remove(id: string): Promise<boolean> {
-    throw new Error('Method not implemented.')
+  async remove(id: string): Promise<boolean> {
+    const hasSalesPDV = await this.repo.findOne(id)
+
+    if (!hasSalesPDV) {
+      return false
+    }
+    await this.repo.delete(hasSalesPDV)
+    return true
   }
 
-  update(id: string, data: string): Promise<SalesPDV | undefined> {
-    throw new Error('Method not implemented.')
+  async update(
+    id: string,
+    data: UpdateSalesInput
+  ): Promise<SalesPDV | undefined> {
+    let hasSalesPDV = await this.repo.findOne(id)
+    if (!hasSalesPDV) {
+      return undefined
+    }
+    await this.repo.update(id, data)
+    hasSalesPDV = await this.repo.findOne(id)
+    return hasSalesPDV
   }
 }
