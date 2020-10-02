472bdd5e9db329b3e6c0b7d66ef8a2c1eedfdc14
Robson Maia <robsonmvieira@gmail.com>
2020-10-02 20:08:45 -0300

    :sparkles: add sales controller

--- src/api/sale-controller.ts
+++ src/api/sale-controller.ts
@@ -1,54 +1,55 @@
-import { Request, Response } from "express";
-import ControllerBase from "./controller-base";
-import { container } from "tsyringe";
-import ListCategoryHandler from "@modules/categories/services/queries/list-category-handler";
-import GetCategoryHandler from "@modules/categories/services/queries/getone-category-handler";
-import CreateCategoryHandler from "@modules/categories/services/commands/create-category-handler";
-import UpdadeCategoryHandler from "@modules/categories/services/commands/update-category-handler";
-import RemoveCategoryHandler from "@modules/categories/services/commands/remove-category-handler";
-import GetCategoryByNameHandler from "@modules/categories/services/queries/get-by-name-category-handler";
+import { Request, Response } from 'express'
+import ControllerBase from './controller-base'
+import { container } from 'tsyringe'
+import GetCategoryHandler from '@modules/categories/services/queries/getone-category-handler'
+import UpdadeCategoryHandler from '@modules/categories/services/commands/update-category-handler'
+import RemoveCategoryHandler from '@modules/categories/services/commands/remove-category-handler'
+import GetCategoryByNameHandler from '@modules/categories/services/queries/get-by-name-category-handler'
+import ListSalesPDVHandler from '@modules/sales/services/queries/sales.listsalesPDV.handler'
+import CreateSalesPDVHandler from '@modules/sales/services/commands/create.salesPDV.handler'
+import { classToClass } from 'class-transformer'
 
 export default class SaleController implements ControllerBase {
   async index(req: Request, res: Response): Promise<Response> {
-   const service = container.resolve(ListCategoryHandler)
-   const categories = await service.handler()
-   return res.status(200).json(categories)
+    const service = container.resolve(ListSalesPDVHandler)
+
+    const salesPDV = await service.handler(req.body)
+    return res.status(200).json(salesPDV)
   }
+
   async one(req: Request, res: Response): Promise<Response> {
-    const {id} = req.params
+    const { id } = req.params
     const service = container.resolve(GetCategoryHandler)
     const hasCategory = await service.handler(id)
     return res.status(200).json(hasCategory)
-
   }
+
   async create(req: Request, res: Response): Promise<Response> {
-    const newCategory = req.body
-    const service = container.resolve(CreateCategoryHandler)
-    const result = await service.handler(newCategory)
+    const newSalesPDV = req.body
+    const service = container.resolve(CreateSalesPDVHandler)
+    const result = await service.handler(classToClass(newSalesPDV))
     return res.status(201).json(result)
-
   }
+
   async update(req: Request, res: Response): Promise<Response> {
-    const {id} = req.params
+    const { id } = req.params
     const data = req.body
     const service = container.resolve(UpdadeCategoryHandler)
     const updatedCategory = await service.handler(id, data)
     return res.status(201).json(updatedCategory)
-
   }
+
   async remove(req: Request, res: Response): Promise<Response> {
-    const {id} = req.params
+    const { id } = req.params
     const service = container.resolve(RemoveCategoryHandler)
     const isDeletedCategory = await service.handler(id)
     return res.status(200).json(isDeletedCategory)
-
   }
+
   async findByName(req: Request, res: Response): Promise<Response> {
     const data = req.params.name
     const service = container.resolve(GetCategoryByNameHandler)
     const hasCategory = await service.handler(data)
     return res.status(200).json(hasCategory)
-
   }
-  
 }
