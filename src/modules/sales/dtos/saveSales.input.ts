0685b734c23ae3f22dac2367c9607f7875e93a9f
Robson Maia <robsonmvieira@gmail.com>
2020-10-02 20:38:08 -0300

    :recycle: update salesInput

--- src/modules/sales/dtos/saveSales.input.ts
+++ src/modules/sales/dtos/saveSales.input.ts
@@ -1,15 +1,9 @@
-import Collaborator from '@modules/collaborators/models/collaborator'
-import Owner from '@modules/owner/models/owner'
-import ProductSale from '@modules/products/models/product'
-
-export interface SaveSalesInput {
+export default interface SaveSalesInput {
   collaborator_id?: string
-  type_of_payment: string
-  sub_total: string
-  descount: number
-  total: string
   owner_id?: string
-  owner?: Owner
-  collaborator?: Collaborator
-  productsPDV: ProductSale[]
+  type_of_payment: string
+  sub_total?: string
+  descount?: number
+  total?: string
+  itemsSalesPDV: []
 }
