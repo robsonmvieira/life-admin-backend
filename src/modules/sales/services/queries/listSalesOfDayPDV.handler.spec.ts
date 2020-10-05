8a16a3af4bfc66ff2179505ded2cbdb28fb19838
Robson Maia <robsonmvieira@gmail.com>
2020-10-04 23:10:39 -0300

    :white_check_mark: add list sales spec

---
+++ src/modules/sales/services/queries/listSalesOfDayPDV.handler.spec.ts
@@ -0,0 +1,55 @@
+import FakeOwnerRepository from '@infra/repositories/fakes/fakeOwnerRepository'
+import FakeSalesPDVRepository from '@infra/repositories/fakes/fakesalesPDVRepository'
+import Permission from '@modules/permissions/models/permission'
+import ListSalesOfDayPDVHandler from './listSalesOfDayPDV.handler'
+
+let fakeSalesRepository: FakeSalesPDVRepository
+let fakeOwnerRepository: FakeOwnerRepository
+let listSalesOfDayPDVHandler: ListSalesOfDayPDVHandler
+
+describe('List Sales of Day', () => {
+  beforeEach(() => {
+    fakeOwnerRepository = new FakeOwnerRepository()
+    fakeSalesRepository = new FakeSalesPDVRepository()
+    listSalesOfDayPDVHandler = new ListSalesOfDayPDVHandler(fakeSalesRepository)
+  })
+  it('should return sales current day list ', async () => {
+    const newOwner = await fakeOwnerRepository.create({
+      name: 'jhon doe',
+      email: 'jd@email',
+      password: 'any',
+      permissions: [],
+      roles: []
+    })
+
+    const data = {
+      type_of_payment: 'debito',
+      owner_id: 'any',
+      created_at: new Date(2020, 9, 4),
+      updated_at: new Date(2020, 9, 4),
+      productsPDV: []
+    }
+
+    const sale1 = await fakeSalesRepository.create(data)
+    const sale2 = await fakeSalesRepository.create(data)
+    const list = await listSalesOfDayPDVHandler.handler(newOwner.id)
+    expect(list).toEqual(
+      expect.arrayContaining([
+        {
+          type_of_payment: 'debito',
+          owner_id: 'any',
+          created_at: new Date(2020, 9, 4),
+          updated_at: new Date(2020, 9, 4),
+          productsPDV: []
+        },
+        {
+          type_of_payment: 'debito',
+          owner_id: 'any',
+          created_at: new Date(2020, 9, 4),
+          updated_at: new Date(2020, 9, 4),
+          productsPDV: []
+        }
+      ])
+    )
+  })
+})
