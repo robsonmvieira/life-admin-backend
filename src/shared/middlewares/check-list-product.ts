e43d12143657b9a10cc3f90fa88ea577a1ebbfca
Robson Maia <robsonmvieira@gmail.com>
2020-10-10 21:13:12 -0300

    :recycle: update list product logic

--- src/shared/middlewares/check-list-product.ts
+++ src/shared/middlewares/check-list-product.ts
@@ -3,25 +3,45 @@ import GetOneCollaboratorHandler from '@modules/collaborators/services/queries/g
 import GetOneOwnerHandler from '@modules/owner/services/queries/getone-owner-handler'
 import { NextFunction, Request, Response } from 'express'
 import { container } from 'tsyringe'
+import { verify } from 'jsonwebtoken'
 
+interface ShapePayload {
+  id: string
+}
 export default async function checkListProductPermission(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
-  const { id } = req.entity
-  const service = container.resolve(GetOneOwnerHandler)
-  const collaboratorService = container.resolve(GetOneCollaboratorHandler)
-  const collaboratorExists = await collaboratorService.handler(id)
-  const ownerExists = await service.handler(id)
-  if (ownerExists) {
+  const cookie = req.headers.cookie
+  if (!cookie) {
+    throw new AppError('Token Not Provider', 401)
+  }
+
+  const [token, trash] = cookie?.split(';')
+  const [rest, auth] = token.split('=')
+  const secret = `${process.env.APP_SECRET}`
+  try {
+    const decoded = verify(auth, secret)
+    const { id } = decoded as ShapePayload
+    const collaboratorService = container.resolve(GetOneCollaboratorHandler)
+    const ownerService = container.resolve(GetOneOwnerHandler)
+    const collaboratorExists = await collaboratorService.handler(id)
+    const ownerExists = await ownerService.handler(id)
+    if (ownerExists) {
+      req.id = ownerExists.id
+      req.name = ownerExists.name
+      return next()
+    }
+    const hasPermission = collaboratorExists?.permissions.find(p =>
+      p.slug.includes('listar-produtos')
+    )
+    if (!hasPermission) {
+      throw new AppError('Você não tem permissão para esse Recurso', 403)
+    }
+
     return next()
+  } catch (error) {
+    throw new AppError('invalid JWT Token', 401, error)
   }
-  const hasPermission = collaboratorExists?.permissions.find(p =>
-    p.slug.includes('listar-produtos')
-  )
-  if (!hasPermission) {
-    throw new AppError('Você não tem permissão para esse Recurso', 403)
-  }
-  return next()
 }
