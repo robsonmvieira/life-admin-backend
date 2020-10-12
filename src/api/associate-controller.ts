933b99e91a70177cb8793d1138af0a53f3fb7184
Robson Maia <robsonmvieira@gmail.com>
2020-10-12 12:15:05 -0300

    :sparkles: update findByLike method from ctl

--- src/api/associate-controller.ts
+++ src/api/associate-controller.ts
@@ -13,7 +13,9 @@ export default class AssociateController {
   async getproductByLike(req: Request, res: Response): Promise<Response> {
     const service = container.resolve(FindProductsByLikes)
     const { terms } = req.query
-    const result = await service.handler(String(terms))
+    const id = 'cc47f84c-2413-4876-8aa3-d969571c2ab9' // id do John Doe do banco em  produção
+    // const id = '9addaa98-ef00-4a21-b781-861b6e8fbc92' // id do John Doe do banco em  desenvolvimento
+    const result = await service.handler(id, String(terms))
     return res.status(200).json(result)
   }
 }
