56d84a6e46b2ecfec1d2ec07c10fd9fe4dd748b0
Robson Maia <robsonmvieira@gmail.com>
2020-10-21 18:03:55 -0300

    :fire: remove uncessary methods

--- src/infra/http/routes/sale-router.ts
+++ src/infra/http/routes/sale-router.ts
@@ -6,7 +6,5 @@ const routes = Router()
 routes.get('/', sale.index)
 routes.get('/:id', sale.one)
 routes.post('/', sale.create)
-routes.put('/:id', sale.update)
-routes.delete('/:id', sale.remove)
 
 export default routes
