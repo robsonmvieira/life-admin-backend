b7598d02f0ea052b93f9d0b8fe9f32e6f0d4f6d1
Robson Maia <robsonmvieira@gmail.com>
2020-10-24 16:00:49 -0300

    :sparkles: add ListClient handler

---
+++ src/modules/clients/services/queries/list-clients-handler.ts
@@ -0,0 +1,12 @@
+import IClientRepository from '@modules/clients/interfaces/IClientRepository'
+import Client from '@modules/clients/models/client'
+import { inject, injectable } from 'tsyringe'
+
+@injectable()
+export default class ListClientHandler {
+  constructor(@inject('ClientRepository') private repo: IClientRepository) {}
+
+  async hendler(owerId: string): Promise<Client[]> {
+    return await this.repo.index(owerId)
+  }
+}
